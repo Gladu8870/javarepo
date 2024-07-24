a8c935d1df267c2cddb9ae08db12e5135e6093f5
USER <USER@DESKTOP-3JEP5L2>
2024-07-24 11:32:37 +0530

    by first sample program in maven

---
+++ firstmavenproj/src/main/java/firstmavenpackage/Mavensample.java
@@ -0,0 +1,11 @@
+package firstmavenpackage;
+
+public class Mavensample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
